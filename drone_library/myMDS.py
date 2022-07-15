88eaad92eec08a00888e7d188c1694d5cd310856
oselin <pierfrancesco.oselin@gmail.com>
2022-07-15 11:26:11 +0000

    Adding mds plots

--- drone_library/myMDS.py
+++ drone_library/myMDS.py
@@ -2,6 +2,7 @@ import numpy as np                        # ndarrys for gridded data
 import matplotlib.pyplot as plt           # for plotting
 import math                               # trigonometry etc.
 import random                             # for randon numbers
+import seaborn as sns                     # for matrix scatter plots
 
 from sklearn.manifold import MDS          # multidimensional scaling
 from sklearn.random_projection import GaussianRandomProjection # random projection
@@ -12,6 +13,7 @@ from sklearn.metrics.pairwise import euclidean_distances
 
 def main():
     random.seed(0)
+    dpalette = sns.color_palette("rocket_r",n_colors = 4)
     
     n = 5
     dp = np.zeros((n,n))
@@ -25,6 +27,15 @@ def main():
     MDS_transformed = embedding.fit_transform(dp)
 
     print(MDS_transformed.shape)
+
+    plt.subplot(121)
+    pairplot = sns.scatterplot(x = MDS_transformed[:,0],y = MDS_transformed[:,1],markers='o',palette = dpalette,edgecolor="black")
+
+    plt.subplot(122)
+    pairplot = sns.scatterplot(x = MDS_transformed[:,0],y = MDS_transformed[:,1],markers='o',edgecolor="black")
+
+    plt.subplots_adjust(left=0.0, bottom=0.0, right=2., top=1.3, wspace=0.3, hspace=0.2,)
+    plt.show()
 
 
 
