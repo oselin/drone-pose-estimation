a5a74216247a1ba7977518907d1cd1fccfa7ec77
Pierfrancesco Oselin <pierfrancescooselin@gmail.com>
2022-07-18 22:17:29 +0200

    Trying to identify flip ambiguity

--- UAV/Robot.py
+++ UAV/Robot.py
@@ -77,3 +77,22 @@ def d_matrix2(platoon):
 
 def rotateMatrix(theta):
     return np.array([[np.cos(theta),np.sin(theta)],[-np.sin(theta),np.cos(theta)]])
+
+
+def g(t):
+    return t - 2*np.pi*np.floor(t/(2*np.pi)+1/2)
+
+
+def theta1i(Sstar,S,index):
+    if index > 0:
+        return g(np.arctan2(Sstar[1,index],Sstar[0,index])-np.arctan2(S[1,index],S[0,index]))
+    else:
+        return None
+
+def THETAi(S,displ,index):
+    deltaX = displ[0]
+    deltaY = displ[1]
+    return np.arctan2(S[1,index],S[0,index]) + np.arctan2(deltaX,deltaY) - np.pi/2
+
+def theta2i(theta1, THETA):
+    return g(theta1 + 2*THETA) 
--- UAV/__init__.py
+++ UAV/__init__.py
@@ -2,3 +2,7 @@ from UAV.Robot import Robot
 from UAV.Robot import d_matrix
 from UAV.Robot import d_matrix2
 from UAV.Robot import rotateMatrix
+from UAV.Robot import g
+from UAV.Robot import theta1i
+from UAV.Robot import THETAi
+from UAV.Robot import theta2i
