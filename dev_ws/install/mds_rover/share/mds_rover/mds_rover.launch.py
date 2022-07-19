c4bd0468ea2327a463d048e120d5c32ab9021854
Giacomo Mutti <giacomo.mutti@studenti.unitn.it>
2022-07-19 21:47:02 +0200

    initial comunication between anchor and drones

--- dev_ws/install/mds_rover/share/mds_rover/mds_rover.launch.py
+++ dev_ws/install/mds_rover/share/mds_rover/mds_rover.launch.py
@@ -6,30 +6,32 @@ from launch import LaunchDescription
 
 def generate_launch_description():
     return LaunchDescription([
-        #Node(
-        #    package='mds_rover',
-        #    namespace='anchor',
-        #    executable='anchor',
-        #    name='anchor',
-        #    arguments=[{"position":{"x":1.0,"y":1.0,"z":0.0},"number_of_drones":5}]
-        #),
+        Node(
+            package='mds_rover',
+            namespace='anchor',
+            executable='anchor',
+            name='anchor',
+            parameters=[{'position': {"x":1.0,"y":1.0,"z":1.0},'number_of_drones': 3}]
+        ),
         Node(
             package='mds_rover',
             namespace='rover1',
             executable='rover',
             name='rover1',
-            parameters=[{'position':{'x':1.0,'y':1.0,'z':0.0},'index':1}]
+            parameters=[{'position': {'x':1.0,'y':1.0,'z':0.0},'index': 1}]
+        ),
+        Node(
+            package='mds_rover',
+            namespace='rover2',
+            executable='rover',
+            name='rover2',
+            parameters=[{'position': {'x':2.0,'y':2.0,'z':0.0},'index': 2}]
+        ),
+        Node(
+            package='mds_rover',
+            namespace='rover3',
+            executable='rover',
+            name='rover3',
+            parameters=[{'position': {'x':3.0,'y':3.0,'z':0.0},'index': 3}]
         )
-        #Node(
-        #    package='mds_rover',
-        #    namespace='rover2',
-        #    executable='rover',
-        #    name='rover2'
-        #),
-        #Node(
-        #    package='mds_rover',
-        #    namespace='rover3',
-        #    executable='rover',
-        #    name='rover3'
-        #)
     ])
--- dev_ws/install/py_pubsub/share/py_pubsub/package.sh
+++ dev_ws/install/py_pubsub/share/py_pubsub/package.sh
@@ -80,7 +80,6 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/py_pubsub/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/py_pubsub/hook/ament_prefix_path.sh"
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/py_pubsub/share/py_pubsub/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
