5c7ff7af2d30a40ddef97bf23a39b08f175e2402
Pierfrancesco Oselin <pierfrancescooselin@gmail.com>
2022-07-21 18:25:37 +0200

    Fixing the fking plot function

--- UAV/Plots.py
+++ UAV/Plots.py
@@ -1,24 +1,13 @@
 import matplotlib.pyplot as plt
-import UAV.Algebra as alg
-import numpy as np
-
-
+import random
 
 
 def plot_points(S, **kwargs):
     
-    match = c = 0
-
-    c_set = col = [[0,0,0]]
-
-    for i in range(len(S[1,:])-1):
-        while col in c_set:
-            col = [np.random.choice(range(256))/256,
-                   np.random.choice(range(256))/256,
-                   np.random.choice(range(256))/256
-            ]
-        c_set.append(col)
-
+    c_set = [(0,0,0)] + [(random.random(),
+                          random.random(),
+                          random.random()) for _ in range(len(S[1,:])-1)]
+    c = 0
     
     nargs = len(kwargs.items())
 
@@ -34,34 +23,14 @@ def plot_points(S, **kwargs):
 
         return
 
-  
-
-    if list(kwargs.items())[-1][0] == 'match':
-        
-        if type(list(kwargs.items())[-1][1]) == int:
-            match = list(kwargs.items())[-1][1]
-        nargs -= 1
     
-    
-    fig,axis = plt.subplots((match+1),nargs, figsize=(5*nargs,4*(match+1)))
+    fig,axis = plt.subplots(1,nargs, figsize=(5*nargs,4))
 
     for title,value in kwargs.items():
 
-        if c >= nargs: break
-        
-        if match:
-            axis[0,c].scatter(value[0,:], value[1,:], color=c_set, alpha=1.0)
-            axis[0,c].scatter(S    [0,:], S    [1,:], color=c_set, alpha=0.2)
-            axis[0,c].set_title('MDS w/ ' + title)
+        axis[c].scatter(value[0,:], value[1,:], color=c_set, alpha=1.0)
+        axis[c].scatter(S    [0,:], S    [1,:], color=c_set, alpha=0.2)
+        axis[c].set_title('MDS w/ ' + title)
 
-            axis[1,c].scatter(alg.match_anchor(S,value)[0,:], alg.match_anchor(S,value)[1,:], color=c_set, alpha=1.0)
-            axis[1,c].scatter(S    [0,:], S    [1,:], color=c_set, alpha=0.2)
-            axis[1,c].set_title('Overlapped anchor, MDS w/ ' + title)
-        
-        else:
-            axis[c].scatter(value[0,:], value[1,:], color=c_set, alpha=1.0)
-            axis[c].scatter(S    [0,:], S    [1,:], color=c_set, alpha=0.2)
-            axis[c].set_title('MDS w/ ' + title)
         c += 1
-
     plt.show()
