1191ef4850e8821e7c508075cdabe902b39fa65c
oselin <pierfrancesco.oselin@gmail.com>
2023-08-04 22:09:00 +0200

    The miracle

--- src/iq_sim/scripts/simulation.sh
+++ src/iq_sim/scripts/simulation.sh
@@ -77,5 +77,5 @@ sleep $((60 * $1))
 # Launch the script main.py for running MDS, plotting the results and guiding the drones
 echo
 echo 'Launching main.py...'
-gnome-terminal --tab -- bash -c "ros2 run iq_sim main.py --ros-args -p n_drones:=$1 -p altitude:=0.0 -p noise_dist_std:=0.0 -p noise_time_std:=0.0"
+gnome-terminal --tab -- bash -c "ros2 run iq_sim main.py --ros-args -p n_drones:=$1 -p altitude:=5.0 -p noise_dist_std:=0.0 -p noise_time_std:=0.0"
 echo 'main.py launched!'
